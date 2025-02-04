0d99bb366de06666b54aa9a2ae6f6e4aa18d5efb
Gabrieldemarco <pedroprepelec@example.com>
2025-02-03 20:23:43 -0500

    cambios

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -8,10 +8,44 @@ from django.urls import reverse
 from django.views import generic
 from django.contrib.auth import login, logout, authenticate
 import logging
+from .models import Course, Enrollment, Question, Choice, Submission
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 # Create your views here.
 
+
+def show_exam_result(request, course_id, submission_id):
+    context = {}
+    course = get_object_or_404(Course, pk=course_id)
+    submission = Submission.objects.get(id=submission_id)
+    choices = submission.choices.all()
+
+    total_score = 0
+    questions = course.question_set.all()  # Suponiendo que el curso tiene preguntas relacionadas
+
+    for question in questions:
+        correct_choices = question.choice_set.filter(is_correct=True)  # Obtener todas las opciones correctas para la pregunta
+        selected_choices = choices.filter(question=question)  # Obtener las opciones seleccionadas por el usuario para la pregunta
+
+        # Verificar si las opciones seleccionadas son las mismas que las opciones correctas
+        if set(correct_choices) == set(selected_choices):
+            total_score += question.grade  # Agregar la calificación de la pregunta solo si se seleccionaron todas las respuestas correctas
+
+    context['course'] = course
+    context['grade'] = total_score
+    context['choices'] = choices
+
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
+
+def submit(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    user = request.user
+    enrollment = Enrollment.objects.get(user=user, course=course)
+    submission = Submission.objects.create(enrollment=enrollment)
+    choices = extract_answers(request)
+    submission.choices.set(choices)
+    submission_id = submission.id
+    return HttpResponseRedirect(reverse(viewname='onlinecourse:exam_result', args=(course_id, submission_id,)))
 
 def registration_request(request):
     context = {}
